d8fe2fce292e2c90c78f30357c5c44deef4da42c
jessicalyruiz <jessicalyr@gmail.com>
2021-11-29 20:16:10 -0500

    segundo intento

--- src/main/java/my/first/project/jy/Animal.java
+++ src/main/java/my/first/project/jy/Animal.java
@@ -5,6 +5,7 @@ public class Animal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi primer priyecto con GIT HUB");
+		System.out.println("segunda linea");
 	}
 
 }
