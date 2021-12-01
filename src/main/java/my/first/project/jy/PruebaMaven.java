956dc9011b636fd3a117da38a4f8b20a25764841
jessicalyruiz <jessicalyr@gmail.com>
2021-11-30 20:28:37 -0500

    maven ok

--- src/main/java/my/first/project/jy/PruebaMaven.java
+++ src/main/java/my/first/project/jy/PruebaMaven.java
@@ -1,8 +1,9 @@
 package my.first.project.jy;
 
-public class pruebaMaven {
+public class PruebaMaven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("mi primer prueba maven");
+		System.out.println("todo Ok");
 	}
 }
