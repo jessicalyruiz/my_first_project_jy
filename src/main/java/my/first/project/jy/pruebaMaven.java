aece3d19c48d7a6f58ad2fdcbc5d577d82afb99e
jessicalyruiz <jessicalyr@gmail.com>
2021-11-30 20:14:22 -0500

    mi primer projecto maven

---
+++ src/main/java/my/first/project/jy/pruebaMaven.java
@@ -0,0 +1,8 @@
+package my.first.project.jy;
+
+public class pruebaMaven {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("mi primer prueba maven");
+	}
+}
